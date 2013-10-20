9bd873c73faf42f3d093cc190e93b14313f083a0
Ethan Reesor <ethan.reesor@firelizzard.com>
2013-10-19 23:00:25 -0400

    Versions & stuff

---
+++ pwnat2/common.h
@@ -0,0 +1,19 @@
+//
+//  common.h
+//  pwnat2
+//
+//  Created by Ethan Reesor on 10/19/13.
+//  Copyright (c) 2013 Ethan. All rights reserved.
+//
+
+#ifndef pwnat2_common_h
+#define pwnat2_common_h
+
+#define MAJOR_VERSION	0
+#define MINOR_VERSION	1
+#define REVISION		0
+#define SUBREVISION		0
+
+#define SERVER_PORT		"45678"
+
+#endif
