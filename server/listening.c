693eb3c30488f999032c8b04401ba2509884a20a
Ethan Reesor <ethan.reesor@firelizzard.com>
2013-10-20 01:02:36 -0400

    Using open_socket

--- server/listening.c
+++ server/listening.c
@@ -6,57 +6,18 @@
 //  Copyright (c) 2013 Ethan. All rights reserved.
 //
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <netdb.h>
+#include "network.h"
 
 int listener(const char * port, int backlog, pid_t * cpid) {
-	int sockfd, retv, yes;
-	struct addrinfo hints, *servinfo, *p;
+	int sockfd;
 	
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE;
-	
-	if (!(retv = getaddrinfo(NULL, port, &hints, &servinfo))) {
-        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(retv));
-        return 1;
+	if (!open_socket(NULL, port, backlog, &sockfd)) {
+		return 1;
 	}
-	
-	for (p = servinfo; p; p = p->ai_next) {
-        if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0) {
-            perror("server: socket");
-            continue;
-        }
-		
-        if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) < 0) {
-            perror("setsockopt");
-            return 1;
-        }
-		
-        if (bind(sockfd, p->ai_addr, p->ai_addrlen) < 0) {
-            close(sockfd);
-            perror("server: bind");
-            continue;
-        }
-		
-        break;
-	}
-	
-    if (!p)  {
-        fprintf(stderr, "server: failed to bind\n");
-        return 2;
-    }
-	
-	freeaddrinfo(servinfo);
 	
     if (listen(sockfd, backlog) == -1) {
         perror("listen");
-        exit(1);
+		return 1;
     }
 	
 	if (!(*cpid = fork()))
