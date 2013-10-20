24e43276ba45b10baabd359c6775c5ca9b41cb64
Ethan Reesor <ethan.reesor@firelizzard.com>
2013-10-20 15:53:47 -0400

    Saving work

--- server/listening.c
+++ server/listening.c
@@ -6,12 +6,52 @@
 //  Copyright (c) 2013 Ethan. All rights reserved.
 //
 
+#include <unistd.h>
+#include <stdio.h>
+
 #include "network.h"
 
-int listener(const char * port, int backlog, pid_t * cpid) {
+int do_handler(int sockfd, struct sockaddr_storage addr, socklen_t addrlen, int acptfd) {
+	int retv = 0;
+	char buf[256];
+	pk_keepalive_t * pk = (pk_keepalive_t *)buf;
+	
+	close(sockfd);
+	
+	if ((retv = recv_handshake(acptfd)))
+		goto close;
+	
+close:
+	close(acptfd);
+	return retv;
+}
+
+int fork_handler(int sockfd, struct sockaddr_storage addr, socklen_t addrlen, int acptfd) {
+	if (!fork())
+		return do_handler(sockfd, addr, addrlen, acptfd);
+	return 0;
+}
+
+int do_listener(int sockfd) {
+	struct sockaddr_storage addr;
+	socklen_t addrlen;
+	int acptfd;
+	
+	while (1) {
+		if ((acptfd = accept(sockfd, (struct sockaddr *)&addr, &addrlen)) < 0) {
+			perror("accept");
+			continue;
+		}
+		
+		fork_handler(sockfd, addr, addrlen, acptfd);
+		close(acptfd);
+	}
+}
+
+int fork_listener(const char * port, int backlog, pid_t * cpid) {
 	int sockfd;
 	
-	if (!open_socket(NULL, port, backlog, &sockfd)) {
+	if (!open_socket(NULL, (char *)port, &sockfd)) {
 		return 1;
 	}
 	
@@ -21,9 +61,7 @@ int listener(const char * port, int backlog, pid_t * cpid) {
 	}
 	
 	if (!(*cpid = fork()))
-		while (1) {
-			
-		}
+		do_listener(sockfd);
 	
 	return 0;
 }
